04c816e6e131507784080465be728fbe1d1f4867
Henry Schmid <henry.schmid1@gmail.com>
2021-05-19 21:30:28 -0600

    Add animal hash

--- hash-examples.rb
+++ hash-examples.rb
@@ -5,3 +5,10 @@ car = {
   can_drive: true,
   features: ['doors', 'wheels', 'wipers']
 }
+
+animal = {
+  animal_type: 'fox',
+  is_mammal: true,
+  num_eyes: 2,
+  appendages: ['legs', 'tail', 'whiskers']
+}
